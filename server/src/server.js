f3c88080953cf5ef59a7dd5ca6b542c9e07c9672
Nafis <abdullahnafis256@gmail.com>
2025-07-10 16:59:58 +0600

    Middleware error handling learned

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const morgan = require('morgan')
+app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-app.listen(3001, ()=>{
-    console.log('server is running at http://localhost:3001');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extented: true}));
 
-app.use(morgan("dev"))
+// Using middleware isLoggedIn:
+const isLoggedIn = (req, res, next) =>{
+    const login = true;
+    if(login){
+        req.user = {id : 101};
+        next();
+    }else{
+        // console.log('You are unauthorized please login again');
+
+        return res.status(401).json({message: 'pleaase login first!'});
+    }
+};
+
+// Using APIs
 
 app.get('/test', (req, res) => {
 
@@ -16,21 +32,51 @@ app.get('/test', (req, res) => {
     });
 });
 
-app.post('/test',(req, res)=> {
+app.get('/api/user',isLoggedIn, (req, res)=>{
 
+    console.log(req.user.id);
     res.status(200).send({
-        message: 'POST: API testing is working fine',
+        message: 'user profile is returned',
     });
 });
-app.put('/test',(req, res)=> {
 
-    res.status(200).send({
-        message: 'PUT: API testing is working fine',
-    });
-})
-app.delete('/test',(req, res)=> {
+//Using client error handling middleware
 
-    res.status(200).send({
-        message: 'DELETE: API testing is working fine',
+app.use((req, res, next) => {
+
+    res.status(404).json({
+        message: 'route not found',
+        
     });
-})
+
+    next();
+
+});
+
+//Using server error handling middleware
+
+
+// app.post('/test',(req, res)=> {
+
+//     res.status(200).send({
+//         message: 'POST: API testing is working fine',
+//     });
+// });
+// app.put('/test',(req, res)=> {
+
+//     res.status(200).send({
+//         message: 'PUT: API testing is working fine',
+//     });
+// })
+// app.delete('/test',(req, res)=> {
+
+//     res.status(200).send({
+//         message: 'DELETE: API testing is working fine',
+//     });
+// })
+
+
+app.listen(3001, ()=>{
+    console.log('server is running at http://localhost:3001');
+});
+
